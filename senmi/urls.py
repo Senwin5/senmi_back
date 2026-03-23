93ec962f36be5be99c98e49ab858f27565abb06a
Senwin.ng <godwinsenwin@outlook.com>
2026-03-23 18:40:10 +0100

    Images are All compres cloud

--- senmi/urls.py
+++ senmi/urls.py
@@ -1,7 +1,7 @@
 # senmi/urls.py
 from django.urls import path
-from .views import RegisterView, RiderProfileUpdateView, CustomLoginView
-from .views import AvailablePackagesView, AcceptPackageView
+from .views import CreatePackageView, RegisterView, RiderEarningsView, RiderProfileUpdateView
+from .views import AvailablePackagesView, AcceptPackageView,UpdateDeliveryStatusView,CustomLoginView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
@@ -9,4 +9,7 @@ urlpatterns = [
     path('rider-profile/', RiderProfileUpdateView.as_view(), name='rider-profile-update'),
     path('packages/', AvailablePackagesView.as_view()),
     path('packages/<int:package_id>/accept/', AcceptPackageView.as_view()),
+    path('create-package/', CreatePackageView.as_view()),
+    path('packages/<int:package_id>/update-status/', UpdateDeliveryStatusView.as_view()),
+    path('rider-earnings/', RiderEarningsView.as_view()),
 ]
