9850a219ac9a76712963fc032ae93bdffc6392c7
Senwin.ng <godwinsenwin@outlook.com>
2026-05-09 06:10:48 +0100

    Work let me update

--- senmi_back/settings.py
+++ senmi_back/settings.py
@@ -205,7 +205,8 @@ FUEL_MULTIPLIER = 1.39   # can increase during fuel hike
 
 
 STATIC_URL = 'static/'
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
