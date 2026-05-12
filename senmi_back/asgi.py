995036145f1bc314398d041e0c3298edf9ffc33f
Senwin.ng <godwinsenwin@outlook.com>
2026-05-12 21:42:39 +0100

    Work let me update

--- senmi_back/asgi.py
+++ senmi_back/asgi.py
@@ -1,6 +1,8 @@
 from urllib.parse import parse_qs
+
 from channels.middleware import BaseMiddleware
 from channels.db import database_sync_to_async
+
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import AnonymousUser
 
@@ -10,11 +12,17 @@ User = get_user_model()
 @database_sync_to_async
 def get_user(token):
 
-    from rest_framework_simplejwt.tokens import AccessToken  # 🔥 MOVE HERE
-
     try:
+        # 🔥 IMPORT INSIDE FUNCTION (prevents Django startup crash)
+        from rest_framework_simplejwt.tokens import AccessToken
+
         access_token = AccessToken(token)
-        user_id = access_token["user_id"]
+
+        user_id = access_token.get("user_id")
+
+        if not user_id:
+            return AnonymousUser()
+
         return User.objects.get(id=user_id)
 
     except Exception as e:
@@ -26,15 +34,21 @@ class JwtAuthMiddleware(BaseMiddleware):
 
     async def __call__(self, scope, receive, send):
 
-        query_string = parse_qs(
-            scope["query_string"].decode()
-        )
+        try:
+            query_string = parse_qs(
+                scope["query_string"].decode()
+            )
 
-        token = query_string.get("token")
+            token_list = query_string.get("token")
 
-        if token:
-            scope["user"] = await get_user(token[0])
-        else:
+            if token_list:
+                token = token_list[0]
+                scope["user"] = await get_user(token)
+            else:
+                scope["user"] = AnonymousUser()
+
+        except Exception as e:
+            print("MIDDLEWARE ERROR:", e)
             scope["user"] = AnonymousUser()
 
         return await super().__call__(scope, receive, send)
