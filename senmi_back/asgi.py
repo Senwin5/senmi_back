403c0b1ffc73def4acaef71371170389f0eb97d4
Senwin.ng <godwinsenwin@outlook.com>
2026-05-12 21:37:26 +0100

    Work let me update

--- senmi_back/asgi.py
+++ senmi_back/asgi.py
@@ -1,21 +1,40 @@
-import os
+from urllib.parse import parse_qs
+from channels.middleware import BaseMiddleware
+from channels.db import database_sync_to_async
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AnonymousUser
 
-from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
+User = get_user_model()
 
-from senmi.routing import websocket_urlpatterns
-from senmi.jwt_middleware import JwtAuthMiddleware
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'senmi_back.settings')
+@database_sync_to_async
+def get_user(token):
 
-django_asgi_app = get_asgi_application()
+    from rest_framework_simplejwt.tokens import AccessToken  # 🔥 MOVE HERE
 
-application = ProtocolTypeRouter({
-    "http": django_asgi_app,
+    try:
+        access_token = AccessToken(token)
+        user_id = access_token["user_id"]
+        return User.objects.get(id=user_id)
 
-    "websocket": JwtAuthMiddleware(
-        URLRouter(
-            websocket_urlpatterns
+    except Exception as e:
+        print("JWT ERROR:", e)
+        return AnonymousUser()
+
+
+class JwtAuthMiddleware(BaseMiddleware):
+
+    async def __call__(self, scope, receive, send):
+
+        query_string = parse_qs(
+            scope["query_string"].decode()
         )
-    ),
-})
+
+        token = query_string.get("token")
+
+        if token:
+            scope["user"] = await get_user(token[0])
+        else:
+            scope["user"] = AnonymousUser()
+
+        return await super().__call__(scope, receive, send)
