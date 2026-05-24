288b4b04ed1119e0114e2e4c22ab4d3296a30792
Senwin.ng <godwinsenwin@outlook.com>
2026-05-24 12:32:04 +0100

    firebase service account secret

--- senmi/firebase.py
+++ senmi/firebase.py
@@ -1,22 +1,4 @@
-import firebase_admin
-from firebase_admin import credentials
 import os
-
-if not firebase_admin._apps:
-
-    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-    cred_path = os.path.join(BASE_DIR, "firebase-service-account.json")
-
-    cred = credentials.Certificate(cred_path)
-
-    firebase_admin.initialize_app(cred)
-
-    print("🔥 FIREBASE INITIALIZED")
-    
-
-
-"""import os
 import json
 import firebase_admin
 from firebase_admin import credentials
@@ -25,11 +7,24 @@ if not firebase_admin._apps:
 
     firebase_json = os.getenv("FIREBASE_CREDENTIALS_JSON")
 
-    if not firebase_json:
-        raise Exception("FIREBASE_CREDENTIALS_JSON not set")
+    if firebase_json:
 
-    cred_dict = json.loads(firebase_json)
+        # Railway / production
+        cred_dict = json.loads(firebase_json)
+        cred = credentials.Certificate(cred_dict)
 
-    cred = credentials.Certificate(cred_dict)
+    else:
 
-    firebase_admin.initialize_app(cred)"""
+        # Local development
+        BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+        cred_path = os.path.join(
+            BASE_DIR,
+            "firebase-service-account.json"
+        )
+
+        cred = credentials.Certificate(cred_path)
+
+    firebase_admin.initialize_app(cred)
+
+    print("🔥 FIREBASE INITIALIZED")
