b34a18c943331942f71add9d4aa6b652814c4596
Senwin.ng <godwinsenwin@outlook.com>
2026-06-01 14:15:32 +0100

    A new role has been added to the databas

--- senmi/admin.py
+++ senmi/admin.py
@@ -102,7 +102,15 @@ class UserAdmin(BaseUserAdmin):
         PackageInline,
         PackageAsRiderInline,
         PackageTrackingInline,
+        
     ]
+    @admin.action(description="Deactivate selected users")
+    def deactivate_users(modeladmin, request, queryset):
+        queryset.update(is_active=False)
+
+    @admin.action(description="Activate selected users")
+    def activate_users(modeladmin, request, queryset):
+        queryset.update(is_active=True)
 
 
 # -----------------------------
