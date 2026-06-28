0986a77c2a3dda2ebe45ba6663bfcd478fb6f96e
Senwin.ng <godwinsenwin@outlook.com>
2026-06-28 14:15:55 +0100

    Admin to receive different email

--- senmi/views.py
+++ senmi/views.py
@@ -1881,43 +1881,6 @@ class PaystackWebhookView(APIView):
                 notify_admin_dashboard()
                 logger.info(f"Package {package.id} marked as paid via webhook.")
 
-                # CUSTOMER EMAIL
-                try:
-                    send_email(
-                        subject="Payment Successful",
-                        message=(
-                            f"Hello {package.customer.username},\n\n"
-                            f"Your payment for package {package.package_id} was successful.\n\n"
-                            f"Delivery Code: {package.delivery_code}\n\n"
-                            f"Please share this code ONLY with the rider upon delivery.\n\n"
-                            f"Your package is now available for riders to accept.\n\n"
-                            f"Thank you for using Senmi."
-                        ),
-                        recipients=[package.customer.email]
-                    )
-                except Exception as e:
-                    logger.exception(f"Customer email failed: {e}")
-
-
-                try:
-                    send_email(
-                        subject="Customer Paid for Package",
-                        message=(
-                            f"A customer has successfully paid.\n\n"
-                            f"Package ID: {package.package_id}\n"
-                            f"Customer: {package.customer.username}\n"
-                            f"Customer Email: {package.customer.email}\n"
-                            f"Pickup: {package.pickup_address}\n"
-                            f"Delivery: {package.delivery_address}\n"
-                            f"Amount: ₦{package.price}\n"
-                            f"Status: {package.status}\n"
-                            f"Delivery Code: {package.delivery_code}\n"
-                        ),
-                        recipients=[settings.NOTIFY_EMAIL]
-                    )
-                except Exception as e:
-                    logger.exception(f"Admin email failed: {e}")
-
         except Package.DoesNotExist:
             logger.warning(f"No package found with payment reference {reference}")
             return Response(status=200)
