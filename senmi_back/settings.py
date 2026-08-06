82e7a90b521dccefacc3c0f33934eb124af1b2ef
Senwin.ng <godwinsenwin@outlook.com>
2026-08-06 13:58:36 +0100

    Testing the biometric

--- senmi_back/settings.py
+++ senmi_back/settings.py
@@ -90,7 +90,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'senmi_back.wsgi.application'
 
 
-
 REDIS_URL = os.environ.get("REDIS_URL")
 
 
@@ -141,11 +140,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
 #TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_TZ = True
 
 
