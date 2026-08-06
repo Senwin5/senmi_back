701a032490658a95f9fbba153b9276713ececbf8
Senwin.ng <godwinsenwin@outlook.com>
2026-08-06 13:13:35 +0100

    Testing the biometric

--- senmi_back/settings.py
+++ senmi_back/settings.py
@@ -156,9 +156,17 @@ REST_FRAMEWORK = {
 }
 
 
+#SIMPLE_JWT = {
+ #   "ACCESS_TOKEN_LIFETIME": timedelta(days=7),
+  #  "REFRESH_TOKEN_LIFETIME": timedelta(days=30),
+   # "ROTATE_REFRESH_TOKENS": False,
+    #"BLACKLIST_AFTER_ROTATION": False,
+    #"AUTH_HEADER_TYPES": ("Bearer",),
+#}
+
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(days=7),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=30),
+    "ACCESS_TOKEN_LIFETIME": timedelta(seconds=30),
+    "REFRESH_TOKEN_LIFETIME": timedelta(minutes=1),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": False,
     "AUTH_HEADER_TYPES": ("Bearer",),
