28ea43a297a5cc7febc41b7066598a9bb16fb9db
Senwin.ng <godwinsenwin@outlook.com>
2026-09-14 01:16:25 +0100

    debug AAB release to git

--- senmi_back/settings.py
+++ senmi_back/settings.py
@@ -18,8 +18,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-#DEBUG = False
+#DEBUG = True
+
+DEBUG = False
 
 
 ALLOWED_HOSTS = [
